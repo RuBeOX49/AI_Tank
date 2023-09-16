e8d538900394538a6d30d8659ff5825744880198
RuBeOX49 <daniel.alonsoherranz@gmail.com>
2023-09-16 18:12:59 +0200

    its joever

--- Assets/Scripts/Managers/TankManager.cs
+++ Assets/Scripts/Managers/TankManager.cs
@@ -8,30 +8,33 @@ public class TankManager
     // It works with the GameManager class to control how the tanks behave
     // and whether or not players have control of their tank in the 
     // different phases of the game.
-
+    
     public Color m_PlayerColor;                             // This is the color this tank will be tinted.
     public Transform m_SpawnPoint;                          // The position and direction the tank will have when it spawns.
+    public Path path;
+    public pathType path_type;
     [HideInInspector] public int m_PlayerNumber;            // This specifies which player this the manager for.
     [HideInInspector] public string m_ColoredPlayerText;    // A string that represents the player with their number colored to match their tank.
     [HideInInspector] public GameObject m_Instance;         // A reference to the instance of the tank when it is created.
     [HideInInspector] public int m_Wins;                    // The number of wins this player has so far.
 
 
-    private TankMovement m_Movement;                        // Reference to tank's movement script, used to disable and enable control.
+    private AITankMovement m_Movement;                        // Reference to tank's movement script, used to disable and enable control.
     private TankShooting m_Shooting;                        // Reference to tank's shooting script, used to disable and enable control.
     private GameObject m_CanvasGameObject;                  // Used to disable the world space UI during the Starting and Ending phases of each round.
-
+    
 
     public void Setup ()
     {
         // Get references to the components.
-        m_Movement = m_Instance.GetComponent<TankMovement> ();
+        m_Movement = m_Instance.GetComponent<AITankMovement> ();
         m_Shooting = m_Instance.GetComponent<TankShooting> ();
         m_CanvasGameObject = m_Instance.GetComponentInChildren<Canvas> ().gameObject;
 
+        m_Movement.sendPathInfo(path, path_type);
         // Set the player numbers to be consistent across the scripts.
         m_Movement.m_PlayerNumber = m_PlayerNumber;
-        m_Shooting.m_PlayerNumber = m_PlayerNumber;
+        //m_Shooting.m_PlayerNumber = m_PlayerNumber;
 
         // Create a string using the correct color that says 'PLAYER 1' etc based on the tank's color and the player's number.
         m_ColoredPlayerText = "<color=#" + ColorUtility.ToHtmlStringRGB(m_PlayerColor) + ">PLAYER " + m_PlayerNumber + "</color>";
@@ -52,7 +55,7 @@ public class TankManager
     public void DisableControl ()
     {
         m_Movement.enabled = false;
-        m_Shooting.enabled = false;
+    //        m_Shooting.enabled = false;
 
         m_CanvasGameObject.SetActive (false);
     }
@@ -62,7 +65,7 @@ public class TankManager
     public void EnableControl ()
     {
         m_Movement.enabled = true;
-        m_Shooting.enabled = true;
+//        m_Shooting.enabled = true;
 
         m_CanvasGameObject.SetActive (true);
     }
--- Assets/Scripts/Tank/AITankMovement.cs
+++ Assets/Scripts/Tank/AITankMovement.cs
@@ -1,5 +1,11 @@
 ﻿using System;
 using UnityEngine;
+public enum pathType
+{
+    path_simple,
+    path_loop,
+    path_patrol,
+}
 
 public class AITankMovement : MonoBehaviour
 {
@@ -14,31 +20,36 @@ public class AITankMovement : MonoBehaviour
 
     private string m_MovementAxisName;          // The name of the input axis for moving forward and back.
     private string m_TurnAxisName;              // The name of the input axis for turning.
-    private Rigidbody m_Rigidbody;              // Reference used to move the tank.
     private float m_MovementInputValue;         // The current value of the movement input.
     private float m_TurnInputValue;             // The current value of the turn input.
     private float m_OriginalPitch;              // The pitch of the audio source at the start of the scene.
 
     [SerializeField] private Path path;
-    [SerializeField] private bool isLooping;
-    [SerializeField] private float slowingDistanceThreshold = 7f;
 
+
+    [SerializeField] private pathType path_type;
+    private bool patrollingFoward = true;
+    [SerializeField] private float slowingDistanceThreshold = 7f;
+    [SerializeField] private float mass = 8f;
+    [SerializeField] private float speed = 5f;
     private int currentPoint = 0;
 
     public int minDistToAvoid = 5;
     public LayerMask layerMask;
 
     private float speedMultiplier = 0.0f;
-    private void Awake ()
+
+    private float currSpeed;
+
+    private Vector3 velocity;
+    private void Awake()
     {
-        m_Rigidbody = GetComponent<Rigidbody> ();
     }
 
 
-    private void OnEnable ()
+    private void OnEnable()
     {
         // When the tank is turned on, make sure it's not kinematic.
-        m_Rigidbody.isKinematic = false;
 
         // Also reset the input values.
         m_MovementInputValue = 0f;
@@ -46,14 +57,13 @@ public class AITankMovement : MonoBehaviour
     }
 
 
-    private void OnDisable ()
+    private void OnDisable()
     {
         // When the tank is turned off, set it to kinematic so it stops moving.
-        m_Rigidbody.isKinematic = true;
     }
 
 
-    private void Start ()
+    private void Start()
     {
         // The axes names are based on player number.
         m_MovementAxisName = "Vertical" + m_PlayerNumber;
@@ -64,74 +74,94 @@ public class AITankMovement : MonoBehaviour
     }
 
 
-    private void Update ()
+    private void Update()
     {
 
-        speedMultiplier += Time.deltaTime;
+        // Calculate current speed
+        currSpeed = speed * Time.deltaTime;
 
-        //Search for the current pathpoint
-        Vector3 currPointPos = path.GetPoint(currentPoint);
-        float newAngle;
-        //Search for any obstacles
-        if(Physics.BoxCast(
-            transform.position - transform.forward,
-            transform.localScale * 1.75f,
-            transform.forward,
-            out RaycastHit hit,
-            Quaternion.identity,
-            minDistToAvoid,
-            layerMask)){
-                //If obstacles, avoid
-                var myHit = hit.normal;
-                myHit.y = 0;
 
-                var myFoward = transform.forward;
-                myFoward.y = 0;
-                newAngle = Vector3.SignedAngle(myFoward, myFoward + myHit, Vector3.up);
-                DrawArrow.ForDebug(transform.position, myFoward+myHit);
-            } else {
-                var myFoward = transform.forward;
-                myFoward.y = 0;
-                newAngle = Vector3.SignedAngle(myFoward, myFoward + (path.GetPoint(currentPoint) - myFoward), Vector3.up);
-                DrawArrow.ForDebug(transform.position,myFoward + (path.GetPoint(currentPoint) - myFoward));
-            }
-        
-        //If near to the pathpoint, search for next pathpoint (if looping)
-                Vector3 distanceToPoint = transform.position - path.GetPoint(currentPoint);
-                if(distanceToPoint.magnitude < path.radius){
-                    Debug.Log("Swapping to next point");
-                    currentPoint++;
-                }
-                if(isLooping)
-                    currentPoint %= path.Length;
-                else {
-                    if(currentPoint == path.Length - 1){
-                        speedMultiplier -= Time.deltaTime;
-                    }
-                }
+        //calculate velocity - Steering
 
-        speedMultiplier = Math.Clamp(speedMultiplier, 0f, 1f);
 
-        m_TurnInputValue = newAngle;
 
-        EngineAudio ();
+        if (currentPoint >= path.Length - 1 && path_type == pathType.path_simple)
+        {
+            velocity += Steer(path.GetPoint(currentPoint), true);
+        }
+        else velocity += Steer(path.GetPoint(currentPoint));
 
-    
+        //Move
+
+
+
+
+
+
+        SearchNextPoint();
+
+        transform.position += velocity;
+
+        //Rotate
+
+        transform.rotation = Quaternion.LookRotation(velocity);
+
+
+        EngineAudio();
+
+
     }
 
+    private void SearchNextPoint()
+    {
+        //If near to the pathpoint, search for next pathpoint based on pathtype
+        Vector3 distanceToPoint = transform.position - path.GetPoint(currentPoint);
+        if (distanceToPoint.magnitude < path.radius)
+        {
+            switch (path_type)
+            {
+                case pathType.path_simple:
+                    if (currentPoint < path.Length - 1)
+                        currentPoint++;
+                    break;
+                case pathType.path_loop:
+                    currentPoint++;
+                    currentPoint %= path.Length;
+                    break;
+                case pathType.path_patrol:
+                    if (patrollingFoward)
+                    {
+                        //its going 0-path.Length - 1;
+                        if (currentPoint == path.Length - 1)
+                            patrollingFoward = false;
+                        else currentPoint++;
+                    }
+                    else
+                    {
+                        if (currentPoint == 0)
+                            patrollingFoward = true;
+                        else currentPoint--;
+                    }
+                    break;
+                default:
+                    currentPoint = 0;
+                    break;
+            }
+        }
+    }
 
-    private void EngineAudio ()
+    private void EngineAudio()
     {
         // If there is no input (the tank is stationary)...
-        if (Mathf.Abs (m_MovementInputValue) < 0.1f && Mathf.Abs (m_TurnInputValue) < 0.1f)
+        if (Mathf.Abs(m_MovementInputValue) < 0.1f && Mathf.Abs(m_TurnInputValue) < 0.1f)
         {
             // ... and if the audio source is currently playing the driving clip...
             if (m_MovementAudio.clip == m_EngineDriving)
             {
                 // ... change the clip to idling and play it.
                 m_MovementAudio.clip = m_EngineIdling;
-                m_MovementAudio.pitch = UnityEngine.Random.Range (m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
-                m_MovementAudio.Play ();
+                m_MovementAudio.pitch = UnityEngine.Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
+                m_MovementAudio.Play();
             }
         }
         else
@@ -147,32 +177,51 @@ public class AITankMovement : MonoBehaviour
         }
     }
 
-
-    private void FixedUpdate ()
+    private Vector3 Steer(Vector3 target, bool isEndpoint = false)
     {
-        
+        Vector3 desiredVelocity = Vector3.zero;
+        if (Physics.BoxCast(
+            transform.position - transform.forward,
+            transform.localScale * 1.75f,
+            transform.forward,
+            out RaycastHit hit,
+            Quaternion.identity,
+            minDistToAvoid,
+            layerMask))
+        {
+            //Get the normal of the hitpoint (of the boxCast)
+            Vector3 hitNormal = hit.normal;
+            hitNormal.y = 0f;
+            desiredVelocity = transform.forward + hitNormal;
+        }
+        else
+        {
+            desiredVelocity = target - transform.position;
+        }
+        float dist = desiredVelocity.magnitude;
+        desiredVelocity = desiredVelocity.normalized;
+
+        if (isEndpoint && dist < slowingDistanceThreshold)
+            desiredVelocity *= (currSpeed * dist / 10f);
+        else desiredVelocity *= currSpeed;
+
+        Vector3 steeringForce = desiredVelocity - velocity;
+        Vector3 acceleration = steeringForce / mass;
+
+        return acceleration;
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    public void sendPathInfo(Path path, pathType type){
+        this.path = path;
+        path_type = type;
     }
 
 
-    private void Move ()
-    {
-        // Create a vector in the direction the tank is facing with a magnitude based on the input, speed and the time between frames.
-        Vector3 movement = transform.forward * speedMultiplier * m_Speed * Time.deltaTime;
-
-        // Apply this movement to the rigidbody's position.
-        m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
-    }
 
 
-    private void Turn ()
-    {
-        // Determine the number of degrees to be turned based on the input, speed and time between frames.
-        float turn = m_TurnInputValue * Time.deltaTime;
-
-        // Make this into a rotation in the y axis.
-        Quaternion turnRotation = Quaternion.Euler (0f, turn, 0f);
-
-        // Apply this rotation to the rigidbody's rotation.
-        m_Rigidbody.MoveRotation (m_Rigidbody.rotation * turnRotation);
-    }
 }
